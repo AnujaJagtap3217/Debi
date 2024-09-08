863ad8df53cf82c1c24363af24d66f9b28963829
anuja.jagtap@apmosys.mahape <anuja.jagtap@apmosys.mahape@APMOSYSLT0502>
2024-09-08 16:31:54 +0530

    Katlon with Jenkin in Linux

---
+++ Test Suites/KatalonJenkinFinal.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatalonJenkinFinal</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9c50f997-1b63-4217-bc71-335d5dfe9225</testSuiteGuid>
+   <testCaseLink>
+      <guid>16956ab8-02ed-4b38-a7fe-5aef1629b203</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Katalon_Jenkin_Final</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
